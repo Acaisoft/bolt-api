d94fb2f130518fc364f8638ef3c3e31abf96399f
Piotr Mońko <piotrek.monko@gmail.com>
2019-02-27 15:16:15 +0100

    remove job status return value for running tests

--- app/deployer/utils.py
+++ app/deployer/utils.py
@@ -56,10 +56,8 @@ def get_test_run_status(execution_id:str):
             return get_test_preparation_job_status(execution_id, str(execution['test_preparation_job_id']))
         else:
             return execution['test_preparation_job_status']
-    elif execution['status'] in (const.TESTRUN_PREPARING_FAILED, const.TESTRUN_FINISHED):
-        return execution['status']
     else:
-        return get_test_job_status(execution['test_job_id'])
+        return execution['status']
 
 
 def can_refresh_test_preparation_job_status():
