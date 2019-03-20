cca540c74df004ee31f4d45cd3ed2930d22ebc55
Piotr Mońko <piotrek.monko@gmail.com>
2019-03-20 16:05:42 +0100

    chg: usr: allow change to repository url if not yet performed

--- app/appgraph/repository.py
+++ app/appgraph/repository.py
@@ -1,3 +1,5 @@
+import json
+
 import graphene
 from flask import current_app
 from gql import gql
@@ -50,6 +52,8 @@ class CreateValidate(graphene.Mutation):
     @staticmethod
     def validate(info, name, repository_url, project_id, type_slug):
         role, user_id = get_request_role_userid(info)
+        assert role in (const.ROLE_ADMIN, const.ROLE_MANAGER), f'{role} user {user_id} cannot create a repository'
+
         gclient = hasura_client(current_app.config)
 
         project_id = str(project_id)
@@ -133,42 +137,75 @@ class UpdateValidate(graphene.Mutation):
 
     class Arguments:
         id = graphene.UUID()
-        name = graphene.String()
+        name = graphene.String(
+            required=False)
+        repository_url = graphene.String(
+            required=False,
+            description='Repository address.')
 
     Output = ValidationInterface
 
     @staticmethod
-    def validate(info, id, name):
+    def validate(info, id, name=None, repository_url=None):
         role, user_id = get_request_role_userid(info)
+
+        assert role in (const.ROLE_ADMIN, const.ROLE_MANAGER), f'{role} user {user_id} cannot update repository'
 
         gclient = hasura_client(current_app.config)
 
-        name = validators.validate_text(name)
+        if name:
+            name = validators.validate_text(name)
 
-        query = gclient.execute(gql('''query ($repoName:String!, $userId:uuid!) {
-            repository(where:{
+        print(json.dumps({
+            'userId': user_id,
+            'repoName': name or '',
+            'repoId': str(id),
+
+        }))
+        query = gclient.execute(gql('''query ($repoName:String!, $repoId:uuid!, $userId:uuid!) {
+            uniqueName: repository(where:{
                 name:{_eq:$repoName},
-                project:{userProjects:{user_id:$userId}}
+                project: {userProjects: {user_id: {_eq:$userId}}}
             }) { id }
+            
+            repository(
+                where:{
+                    id:{_eq:$repoId},
+                    project:{userProjects:{user_id:{_eq:$userId}}}
+                }
+            ) {
+                performed
+                configurations_aggregate (where:{performed:{_eq:true}}) {
+                    aggregate {
+                        count
+                    }
+                }
+            }
             
             }'''), variable_values={
             'userId': user_id,
-            'repoName': name,
+            'repoName': name or '',
+            'repoId': str(id),
 
         })
-        assert len(query.get('repository')) == 0, f'repository with this name already exists'
+        assert len(query.get('repository')) == 1, f'repository {str(id)} does not exists'
 
-        if role != const.ROLE_ADMIN:
-            assert query.get('project'), \
-                f'non-admin ({role}) user {user_id} does not have access to repository {id}'
+        query_data = {}
 
-        assert len(query.get('configuration_type', [])) == 1, f'configuration type does not exist'
+        if name:
+            assert len(query.get('uniqueName')) == 0, f'repository with this name already exists'
+            query_data['name'] = name
 
-        return {
-            'name': name,
-        }
+        if repository_url:
+            num_performed = query['repository'][0]['configurations_aggregate']['aggregate']['count']
+            assert num_performed == 0, \
+                f'cannot update url, repository is in use by {num_performed} configuration{"s" if num_performed > 1 else ""}'
+            repository_url = validators.validate_accessibility(current_app.config, repository_url)
+            query_data['url'] = repository_url
 
-    def mutate(self, info, id, name):
+        return query_data
+
+    def mutate(self, info, id, name=None, repository_url=None):
         UpdateValidate.validate(info, id, name)
         return ValidationResponse(ok=True)
 
@@ -178,21 +215,21 @@ class Update(UpdateValidate):
 
     Output = OutputInterfaceFactory(RepositoryInterface, 'Update')
 
-    def mutate(self, info, id, name):
+    def mutate(self, info, id, name=None, repository_url=None):
         gclient = hasura_client(current_app.config)
 
-        query_params = UpdateValidate.validate(info, id, name)
+        query_params = UpdateValidate.validate(info, id, name, repository_url)
 
         query = gql('''mutation ($id:uuid!, $data:repository_set_input!) {
             update_repository(
                 where:{id:{_eq:$id}},
                 _set: $data
             ) {
-                returning { id name } 
+                returning { id name repository_url:url project_id type_slug } 
             }
         }''')
 
-        query_response = gclient.execute(query, update={'id': str(id), 'data': query_params})
+        query_response = gclient.execute(query, variable_values={'id': str(id), 'data': query_params})
         assert query_response['update_repository'], f'cannot save repository ({str(query_response)})'
 
         return OutputValueFromFactory(Update, query_response['update_repository'])
