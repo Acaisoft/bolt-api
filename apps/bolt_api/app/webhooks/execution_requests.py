99695ce40c398025461f2144b66a01509b62fc59
Piotr Mońko <piotrek.monko@gmail.com>
2019-04-30 13:42:22 +0200

    remove total calculations on delete

--- apps/bolt_api/app/webhooks/execution_requests.py
+++ apps/bolt_api/app/webhooks/execution_requests.py
@@ -12,32 +12,32 @@ bp = Blueprint('webhooks_execution_requests', __name__)
 @bp.route('/insert', methods=['POST'])
 def update_execution_requests_stats_totals():
     event = request.get_json().get('event')
-    execution_id = None
 
-    if event.get('op') == 'INSERT':
-        new = event.get('data', {}).get('new')
-        del new['id']
-        execution_id = new['execution_id']
+    if event.get('op') != 'INSERT':
+        # do not calculate on DELETE as hasura will send an event for each row of a deleted table
+        return jsonify({})
 
-        response = hce(current_app.config, '''mutation ($data:[execution_request_totals_insert_input!]!) {
-            insert_execution_request_totals(
-                objects: $data,
-                on_conflict: {
-                    constraint: execution_request_totals_pkey
-                    update_columns: [
-                        average_content_size, average_response_time, max_response_time, median_response_time, 
-                        min_response_time, num_failures, num_requests, requests_per_second, timestamp
-                    ]
-                }
-            ) { affected_rows }
-        }''', variable_values={
-            'data': new,
-        })
-        err = response.get('errors', None)
-        if err is not None:
-            logger.warn(f'error inserting execution request totals on execution: {str(err)}')
-    elif event.get('op') == 'DELETE':
-        execution_id = event['data']['old']['execution_id']
+    new = event.get('data', {}).get('new')
+    del new['id']
+    execution_id = new['execution_id']
+
+    response = hce(current_app.config, '''mutation ($data:[execution_request_totals_insert_input!]!) {
+        insert_execution_request_totals(
+            objects: $data,
+            on_conflict: {
+                constraint: execution_request_totals_pkey
+                update_columns: [
+                    average_content_size, average_response_time, max_response_time, median_response_time, 
+                    min_response_time, num_failures, num_requests, requests_per_second, timestamp
+                ]
+            }
+        ) { affected_rows }
+    }''', variable_values={
+        'data': new,
+    })
+    err = response.get('errors', None)
+    if err is not None:
+        logger.warn(f'error inserting execution request totals on execution: {str(err)}')
 
     response = hce(current_app.config, '''query ($execution_id:uuid!) {
         execution_request_totals_aggregate(where:{execution_id:{_eq:$execution_id}}){
