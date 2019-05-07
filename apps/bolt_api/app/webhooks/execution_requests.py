725a92eee1bcf78b83f1d68e39b10acf7df683cc
Piotr Mońko <piotrek.monko@gmail.com>
2019-05-07 15:32:51 +0200

    add min_/max_ from avg resp size to request_totals

--- apps/bolt_api/app/webhooks/execution_requests.py
+++ apps/bolt_api/app/webhooks/execution_requests.py
@@ -1,4 +1,3 @@
-import math
 from flask import Blueprint, request, jsonify, current_app
 
 from services.hasura import hce
@@ -20,8 +19,9 @@ def update_execution_requests_stats_totals():
     new = event.get('data', {}).get('new')
     del new['id']
     execution_id = new['execution_id']
+    req_identifier = new['identifier']
 
-    response = hce(current_app.config, '''mutation ($data:[execution_request_totals_insert_input!]!) {
+    totals_response = hce(current_app.config, '''mutation ($data:[execution_request_totals_insert_input!]!) {
         insert_execution_request_totals(
             objects: $data,
             on_conflict: {
@@ -35,11 +35,12 @@ def update_execution_requests_stats_totals():
     }''', variable_values={
         'data': new,
     })
-    err = response.get('errors', None)
+    err = totals_response.get('errors', None)
     if err is not None:
         logger.warn(f'error inserting execution request totals on execution: {str(err)}')
+        return jsonify({})
 
-    response = hce(current_app.config, '''query ($execution_id:uuid!) {
+    response = hce(current_app.config, '''query ($execution_id:uuid!, $identifier:String!) {
         execution_request_totals_aggregate(where:{execution_id:{_eq:$execution_id}}){
             aggregate {
                 sum {num_requests}
@@ -50,19 +51,41 @@ def update_execution_requests_stats_totals():
                 max {max_response_time}
             }
         }
+        
+        execution_requests_aggregate(where:{
+            execution_id:{ _eq:$execution_id }
+            identifier:{ _eq:$identifier }
+        }){
+            aggregate {
+                min {average_content_size}
+                max {average_content_size}
+            }
+        }
     }''', variable_values={
         'execution_id': execution_id,
+        'identifier': req_identifier,
     })
     err = response.get('errors', None)
     if err is not None:
         logger.warn(f'error calculating request totals on execution: {str(err)}')
+        return jsonify({})
 
     aggs = response['execution_request_totals_aggregate']['aggregate']
     total = aggs['sum']['num_requests']
     fails = aggs['sum']['num_failures']
+    max_content_size = response['execution_requests_aggregate']['aggregate']['max']['average_content_size']
+    min_content_size = response['execution_requests_aggregate']['aggregate']['min']['average_content_size']
 
     if total is not None and fails is not None:
-        response = hce(current_app.config, '''mutation ($execution_id:uuid!, $total_requests:Int!, $passed_requests:Int!, $failed_requests:Int!) {
+        response = hce(current_app.config, '''mutation (
+            $identifier:String!,
+            $execution_id:uuid!, 
+            $total_requests:Int!, 
+            $passed_requests:Int!, 
+            $failed_requests:Int!,
+            $max_content_size:numeric!,
+            $min_content_size:numeric!,
+        ) {
             update_execution(
                 where:{id:{_eq:$execution_id}}
                 _set:{
@@ -71,11 +94,26 @@ def update_execution_requests_stats_totals():
                     failed_requests: $failed_requests
                 }
             ) { affected_rows }
+            
+            update_execution_request_totals(
+                where:{
+                    execution_id:{ _eq:$execution_id }
+                    identifier:{ _eq:$identifier }
+                }
+                _set:{
+                    max_content_size: $max_content_size
+                    min_content_size: $min_content_size
+                }
+            ) { affected_rows }
+            
         }''', variable_values={
             'execution_id': execution_id,
+            'identifier': req_identifier,
             'total_requests': total,
             'failed_requests': fails,
             'passed_requests': int(total) - int(fails),
+            'max_content_size': max_content_size,
+            'min_content_size': min_content_size,
         })
         err = response.get('errors', None)
         if err is not None:
