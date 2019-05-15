df07a31b0e30b7511f8e09fe82fdc7a609a860f5
Piotr Mońko <piotrek.monko@gmail.com>
2019-05-15 09:38:14 +0200

    update bolt-deployer sdk, add charts conf data storage table

--- services/exports/tests/test_data_extractor.py
+++ services/exports/tests/test_data_extractor.py
@@ -1,17 +1,38 @@
 import json
+import os
 import unittest
 
 from services.exports import data_extractor
 
 
-class TestTestCreatorValidation(unittest.TestCase):
+class TestDataExtractor(unittest.TestCase):
 
     @staticmethod
     def _get_fixture(fixture_path):
-        with open(fixture_path, 'r') as f:
+        here = os.path.dirname(__file__)
+        path = os.path.join(here, 'fixtures', fixture_path)
+        with open(path, 'r') as f:
             return json.load(f)
 
     def test_nfs_extractor(self):
-        nfs = self._get_fixture('fixtures/execution_additional_data_nfs.json')
+        """
+        Assert that nfs extension additional data results are correctly converted from database representation of
+            [{[{data}, {more data}, {...}]}, {[{another data}, {more another data}, {...}]}]
+        to a flat
+            [{data}, {more data}, {...}, {another data}, {more another data}, {...}]
+        """
+        nfs = self._get_fixture('execution_additional_data_nfs.json')
         out = data_extractor.convert_data('nfs', [{'data': nfs}])
+        # assert each element is a flat dict of ints or floats and sorted by timestamp
+        prev_ts = 0
+        for i in out:
+            assert type(i) is dict, f'expected a dict, got {str(type(i))}'
+
+            ts = i.get('timestamp', None)
+            assert type(ts) in (int, float), f'timestamp missing or invalid type'
+            assert prev_ts > ts, f'expected output to be ordered by timestamp'
+            prev_ts = ts
+
+            
+
         print(out)
