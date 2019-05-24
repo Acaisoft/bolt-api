c737a8592e71a790b4d48859a5a8033d33fa1037
Piotr Mońko <piotrek.monko@gmail.com>
2019-05-24 16:28:58 +0200

    remove retalimiting from webhooks

--- apps/bolt_api/app/webhooks/__init__.py
+++ apps/bolt_api/app/webhooks/__init__.py
@@ -1,6 +1,3 @@
-from flask import request, jsonify, current_app
-from flask_limiter import Limiter
-
 from . import execution
 from . import configuration_param
 from . import execution_requests
@@ -10,14 +7,6 @@ def register_app(app):
     """
     These register private callbacks for hasura to push events to. Must be private.
     """
-    limiter = Limiter(app, key_func=request_limiter)
-    limiter.limit('10 per second')(execution.bp)
-    limiter.limit('2 per second')(configuration_param.bp)
-    limiter.limit('2 per second')(execution_requests.bp)
     app.register_blueprint(execution.bp, url_prefix='/webhooks/execution')
     app.register_blueprint(configuration_param.bp, url_prefix='/webhooks/configuration_param')
     app.register_blueprint(execution_requests.bp, url_prefix='/webhooks/execution_requests')
-
-
-def request_limiter():
-    return request.path
