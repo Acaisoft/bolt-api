bbd36b2e05fde207f2b9c77f6765c8dc870546d5
art.barysevich <art.barysevich@gmail.com>
2019-07-02 09:14:56 +0200

    Add event trigger for catching stage statuses and saving to graph data

---
+++ apps/bolt_api/app/webhooks/tests/test_execution_stage_graph.py
@@ -0,0 +1,10 @@
+import unittest
+
+from schematics import exceptions
+
+from apps.bolt_api.app.webhooks.execution_stage_log import Item, Graph
+
+
+class TestItemModel(unittest.TestCase):
+    def test_model_data(self):
+        pass
