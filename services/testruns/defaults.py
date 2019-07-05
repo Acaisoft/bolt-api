fbe286014ae716bfc33d7bcf2cd10157d7f84483
art.barysevich <art.barysevich@gmail.com>
2019-07-05 09:58:22 +0200

    Add new default charts by default

--- services/testruns/defaults.py
+++ services/testruns/defaults.py
@@ -4,109 +4,219 @@ DEPLOYER_TIMEOUT = 10
 
 # execution_metrics_metadata is populated with this default at testrun startup
 # TODO: remove or adjust when frontend has a drag-n-drop interface ready for this
-DEFAULT_CHART_CONFIGURATION = '''{
-  "charts": [
-    {
-      "title": "Global Throughput",
-      "type": "line",
-      "node_name": "global_throughput",
-      "x_data_key": "timestamp",
-      "x_format": "number",
-      "y_data_key": "value",
-      "y_format": "number",
-      "y_label": "name"
-    },
-    {
-      "title": "Global CPU usage",
-      "type": "line",
-      "node_name": "global_cpu",
-      "x_data_key": "timestamp",
-      "x_format": "number",
-      "y_data_key": "value",
-      "y_format": "percent",
-      "y_label": "name"
-    },
-    {
-      "title": "Global Memory Usage",
-      "type": "line",
-      "node_name": "global_memory",
-      "x_data_key": "timestamp",
-      "x_format": "number",
-      "y_data_key": "value",
-      "y_format": "bytes",
-      "y_label": "name"
-    },
-    {
-      "title": "Global Disk Usage",
-      "type": "line",
-      "node_name": "global_disc_data",
-      "x_data_key": "timestamp",
-      "x_format": "number",
-      "y_data_key": "value",
-      "y_format": "bytes",
-      "y_label": "name"
-    },
-    {
-      "title": "Source Input per App",
-      "type": "line",
-      "node_name": "apps_source_input",
-      "x_data_key": "timestamp",
-      "x_format": "number",
-      "y_data_key": "source_input",
-      "y_format": "number",
-      "y_label": "app_name"
-    },
-    {
-      "title": "Threads per App",
-      "type": "line",
-      "node_name": "apps_threads",
-      "x_data_key": "timestamp",
-      "x_format": "number",
-      "y_data_key": "threads_num",
-      "y_format": "number",
-      "y_label": "app_name"
-    },
-    {
-      "title": "CPU Usage per App",
-      "type": "line",
-      "node_name": "apps_cpu",
-      "x_data_key": "timestamp",
-      "x_format": "number",
-      "y_data_key": "cpu",
-      "y_format": "percent",
-      "y_label": "app_name"
-    },
-    {
-      "title": "WActions per App",
-      "type": "line",
-      "node_name": "apps_wactions",
-      "x_data_key": "timestamp",
-      "x_format": "number",
-      "y_data_key": "wactions_created",
-      "y_format": "number",
-      "y_label": "app_name"
-    },
-    {
-      "title": "Latency per App",
-      "type": "line",
-      "node_name": "apps_latency",
-      "x_data_key": "timestamp",
-      "x_format": "number",
-      "y_data_key": "latency",
-      "y_format": "number",
-      "y_label": "app_component"
-    },
-    {
-      "x_format": "number",
-      "node_name": "apps_backpressed",
-      "x_data_key": "timestamp",
-      "y_format": "bool",
-      "y_label": "app_name",
-      "title": "Backpressured per App",
-      "type": "heatmap",
-      "y_data_key": "backpressured"
-    }
-  ]
+DEFAULT_CHART_CONFIGURATION = '''{  
+   "charts":[  
+      {  
+         "x_format":"number",
+         "node_name":"global_throughput",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"name",
+         "title":"Global throughput (events/s)",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"nodes",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"name",
+         "title":"Nodes messages/second",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"lag",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"name",
+         "title":"Kafka consumer lag",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"kafka_messages",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"name",
+         "title":"Kafka messages count",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"global_cpu",
+         "x_data_key":"timestamp",
+         "y_format":"percent",
+         "y_label":"name",
+         "title":"CPU usage (Apps + Server) (Striim)",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"vm_cpu",
+         "x_data_key":"timestamp",
+         "y_format":"percent",
+         "y_label":"name",
+         "title":"CPU Usage (Prometheus)",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"vm_ram",
+         "x_data_key":"timestamp",
+         "y_format":"percent",
+         "y_label":"name",
+         "title":"Memory usage (Prometheus)",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"global_memory",
+         "x_data_key":"timestamp",
+         "y_format":"bytes",
+         "y_label":"name",
+         "title":"Memory usage (Striim)",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"global_disc_data",
+         "x_data_key":"timestamp",
+         "y_format":"bytes",
+         "y_label":"name",
+         "title":"Disc space (Striim)",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"vm_disc_usage",
+         "x_data_key":"timestamp",
+         "y_format":"percent",
+         "y_label":"name",
+         "title":"Disc usage (Prometheus)",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"vm_disc",
+         "x_data_key":"timestamp",
+         "y_format":"bytes",
+         "y_label":"name",
+         "title":"Disc space (Prometheus)",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"vm_io",
+         "x_data_key":"timestamp",
+         "y_format":"bytes",
+         "y_label":"name",
+         "title":"I/O (Prometheus)",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"apps_source_input",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"app_name",
+         "title":"Source input per App",
+         "type":"line",
+         "y_data_key":"source_input"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"apps_threads",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"app_name",
+         "title":"CPU thread per app",
+         "type":"line",
+         "y_data_key":"threads_num"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"apps_cpu",
+         "x_data_key":"timestamp",
+         "y_format":"percent",
+         "y_label":"app_name",
+         "title":"CPU usage per App (Striim)",
+         "type":"line",
+         "y_data_key":"cpu"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"apps_wactions",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"app_name",
+         "title":"WebActions per App (Striim)",
+         "type":"line",
+         "y_data_key":"wactions_created"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"number_of_apps",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"name",
+         "title":"Number of running apps",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"apps_latency",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"app_component",
+         "title":"Latency per App (ms)",
+         "type":"line",
+         "y_data_key":"latency"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"apps_backpressed",
+         "x_data_key":"timestamp",
+         "y_format":"bool",
+         "y_label":"app_name",
+         "title":"Backpressured per App",
+         "type":"heatmap",
+         "y_data_key":"backpressured"
+      },
+      {  
+         "x_format":"number",
+         "node_name":"elastic_search_docs",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"name",
+         "title":"Number of Elastic Search Docs",
+         "type":"line",
+         "y_data_key":"value"
+      },
+      {  
+         "x_format":"bytes",
+         "node_name":"elastic_search_size",
+         "x_data_key":"timestamp",
+         "y_format":"number",
+         "y_label":"name",
+         "title":"Elastic Search Size",
+         "type":"line",
+         "y_data_key":"value"
+      }
+   ]
 }'''
 
 # chart conf for configuration with NFS extension
