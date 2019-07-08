8aa4c667ac5a7c9d1db6854708f1e1ba533e30cc
Krystian Kulgawczuk (kkulgawc) <Krystian.Kulgawczuk@quest.com>
2019-07-08 14:05:06 +0200

    ACBT-366 split up assertion based on list len

--- apps/bolt_api/app/appgraph/execution_metrics/tests/test_execution_metadata_generation.py
+++ apps/bolt_api/app/appgraph/execution_metrics/tests/test_execution_metadata_generation.py
@@ -7,4 +7,7 @@ from services.execution_metrics.metadata_execution import _even_select
 @given(sequence=st.lists(st.integers()), list_len_limit=st.integers(min_value=1, max_value=10000))
 def test_even_select(sequence, list_len_limit):
     evenly_selected_len = len(list(_even_select(list(sequence), list_len_limit)))
-    assert evenly_selected_len == list_len_limit or evenly_selected_len == len(sequence)
+    if len(sequence) > list_len_limit:
+        assert evenly_selected_len == list_len_limit
+    else:
+        assert evenly_selected_len == len(sequence)
