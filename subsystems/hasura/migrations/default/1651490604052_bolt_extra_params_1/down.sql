dca1647165bb12a7355ddcbac003212f096d0613
Jerzy Goebel <jerzy.goebel@acaisoft.com>
2022-05-02 13:25:01 +0200

    - adding file_path parameter + migration - adding repository_branch parameter + migration - adding necessary validators

---
+++ subsystems/hasura/migrations/default/1651490604052_bolt_extra_params_1/down.sql
@@ -0,0 +1,5 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- INSERT INTO parameter (name, param_name, param_type, slug_name, default_value) VALUES
+--     ('file path', '-f', 'str', 'load_tests_file_path', 'tests/load_tests.py'),
+--     ('repository branch', '-b', 'str', 'load_tests_repository_branch', 'master');
---
+++ subsystems/hasura/migrations/default/1651490604052_bolt_extra_params_1/up.sql
@@ -0,0 +1,3 @@
+INSERT INTO parameter (name, param_name, param_type, slug_name, default_value) VALUES
+    ('file path', '-f', 'str', 'load_tests_file_path', 'tests/load_tests.py'),
+    ('repository branch', '-b', 'str', 'load_tests_repository_branch', 'master');
