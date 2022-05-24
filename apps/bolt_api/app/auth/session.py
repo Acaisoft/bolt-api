6d234bebfa5d37d5e4d59b6d68d79a9a9adcb46a
Jerzy Goebel <jerzy.goebel@acaisoft.com>
2022-05-24 08:49:38 +0200

    - trying to fix cors on locally hosted api on mac

--- apps/bolt_api/app/auth/session.py
+++ apps/bolt_api/app/auth/session.py
@@ -11,11 +11,10 @@ bp = Blueprint('auth-session', __name__)
 def session():
     token = request.cookies.get('AUTH_TOKEN', False)
     if not token:
-        response = make_response('Missing AUTH_TOKEN cookie')
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        return response, 401
+        response = make_response(Unauthorized('Missing AUTH_TOKEN cookie'))
+    else:
+        response = make_response({'AUTH_TOKEN': token})
+        response.set_cookie('AUTH_TOKEN', expires=0)
 
-    response = make_response({'AUTH_TOKEN': token})
-    response.set_cookie('AUTH_TOKEN', expires=0)
-
+    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
