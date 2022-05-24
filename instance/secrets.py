77aefc510f9d48fc7cd889f905f313d9ee470ba9
Jerzy Goebel <jerzy.goebel@acaisoft.com>
2022-05-24 16:09:01 +0200

    - cleaning up

--- instance/secrets.py
+++ instance/secrets.py
@@ -1,8 +1,3 @@
-
-SECRET_KEY = 'slkd1o8wnO*#LUFHNO4islkd1o8wnO*#'
-
 HASURA_GRAPHQL_ACCESS_KEY = "devaccess"
 
 SECRETS_VERSION = '01beta'
-
-# SENTRY_DSN = 'https://4c646c92944b44fe8f062e2a8cd0bce2:19dff88f1e5e4d84acc94eb0278f8ac5@sentry.tools.acaisoft.io/4'
